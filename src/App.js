c2ea137ffbcd7e7d1d56dfe1604a9b9043e45bc8
saiUppa <saiuppalapati96@gmail.com>
2024-03-26 11:28:09 +0530

    sallonapp demo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      fghjk
+      <h1>Sallon shop</h1>
     </div>
   );
 }
---
+++ src/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+const Header=()=>{
+    return(
+        <>
+        <h1>Hello</h1>
+        </>
+    )
+}
+export default Header;
+class extends React.Component{
+    constructor(){
+        super(){
+            this.render(){
+                
+            }
+        }
+    }
+}
